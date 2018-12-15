925353af2239c09e1aa410a4e53a7dee7f971f54
BaccanoEva <874295953@qq.com>
2018-12-15 19:13:47 +0800

    I think I am dying OTZ

--- app/helpers/texts_helper.rb
+++ app/helpers/texts_helper.rb
@@ -1,5 +1,19 @@
+require 'rubygems'
+require 'json'
+require 'pp'
 module TextsHelper
   def recommendation_for(text)
-    result = `python /Users/liuyancen/Desktop/test.py `+text
+    #result = system `python /Users/liuyancen/Desktop/test.py`,"try"
+    #result = system("python /Users/liuyancen/Desktop/test.py try")
+    #python_cmd = Escape.shell_command(['python', "/Users/liuyancen/Desktop/test.py"]).to_s
+    #system python_cmd
+    #print python_cmd
+    a = text.question
+    pp a
+    result = `python /Users/liuyancen/Desktop/test.py '#{a}'`
+
+    return JSON.parse(File.read('/Users/liuyancen/Desktop/test.json'))
+    #return JSON.parse(json)
+    #return obj
   end
 end
